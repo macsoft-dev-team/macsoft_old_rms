7c2abc8f8c9dd74c91da1de0c03e3e3fc8270612
theboywidsmile <manirud0077@gmail.com>
2025-06-23 09:48:40 +0530

    chore: update dependencies and refactor styles
    - Updated Bootstrap from 5.3.6 to 5.3.7 in package.json.
    - Added MQTT library to package.json.
    - Converted SCSS files to CSS and updated imports in main.jsx.
    - Removed old SCSS files for sidenav and device configuration.
    - Created new CSS files for custom styles, sidenav, and device configuration.
    - Refactored ReduxProvider to a new location in utils/hooks.
    - Updated Prisma schema to include new fields in MqttCredentials and added a new customer model.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
- import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/custom-style.scss';
- import ReduxProvider from './hooks/lib/ReduxProvider.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/custom-style.css';
+import './styles/sidenav.css';
+import './styles/deviceConfiguration.css';
+import ReduxProvider from './utils/hooks/ReduxProvider';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ReduxProvider>
