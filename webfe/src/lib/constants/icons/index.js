e0cbfd9e0869d60f359df16b5008683775cef130
theboywidsmile <manirud0077@gmail.com>
2025-08-06 23:23:47 +0530

    feat: add DeviceMetricCard component and integrate into MetricsGrid
    - Created a new DeviceMetricCard component for displaying device metrics with animations and icons.
    - Updated MetricsGrid to import DeviceMetricCard from the correct path.

---
+++ webfe/src/lib/constants/icons/index.js
@@ -0,0 +1,21 @@
+import energizer from './energizing.json';
+import solarPower from './solar-sun-power-animation.json';
+import waterWaves from './water-waves.json';
+import cFan from "./centrifugal-fan.json";
+import rPm from "./rpm.json";
+import ev from "./ev.json";
+import spark from "./spark.json";
+import turbo from "./turbo.json";
+import thermo from "./thermometer.json";
+
+export const icons = {
+  energizer,
+  solarPower,
+  waterWaves,
+  cFan,
+  rPm,
+  ev,
+  spark,
+  turbo,
+  thermo,
+ };
