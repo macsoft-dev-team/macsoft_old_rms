934dfab74a667659ff95a1fe9ef859f139c01b24
rajasekaran-sbss <rajasekaran.sbss@gmail.com>
2025-12-23 16:57:46 +0530

    feat: Add MQTT username, password, and client ID environment variables.

--- rms-jobs/ecosystem.config.js
+++ rms-jobs/ecosystem.config.js
@@ -15,6 +15,9 @@ module.exports = {
         "JWT_SECRET_KEY": "mqtt-secret",
         "MQTT_HOST": "localhost",
         "MQTT_PORT": 1883,
+        "MQTT_USERNAME": "admin",
+        "MQTT_PASSWORD": "admin",
+        "MQTT_CLIENT_ID": "rms-device-listener",
         "LOG_LEVEL": 'info'
       },
       "env_development": {
@@ -24,6 +27,9 @@ module.exports = {
         "JWT_SECRET_KEY": "mqtt-secret",
         "MQTT_HOST": "localhost",
         "MQTT_PORT": 1883,
+        "MQTT_USERNAME": "admin",
+        "MQTT_PASSWORD": "admin",
+        "MQTT_CLIENT_ID": "rms-device-listener",
         "LOG_LEVEL": 'debug'
       },
       "error_file": './logs/device-listener-error.log',
